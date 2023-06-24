1f33ea5260d862b90b15e839013056dfdca89060
Paij0 <gustikawulane794@gmail.com>
2023-06-24 10:12:18 +0700

    first commit

---
+++ mqtt-publish.py
@@ -0,0 +1,38 @@
+import paho.mqtt.client as paho
+from paho import mqtt
+from time import sleep
+
+# define static variable
+# broker = "localhost" # for local connection
+broker = "broker.hivemq.com"  # for online version
+port = 1883
+timeout = 60
+
+username = 'adara'
+password = '123'
+topic = "sic"
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+ 
+def on_publish(client,userdata,result):
+	print("data published \n")
+	
+
+
+client1 = paho.Client("device1",userdata=None,protocol=paho.MQTTv5)
+client1.username_pw_set(username=username,password=password)
+client1.on_connect = on_connect
+client1.on_publish = on_publish
+client1.connect(broker,port,timeout)
+
+count = 0
+while count < 5:
+	status = str(input("Do you want the lamp turn On or Off? "))
+	message = status
+	ret = client1.publish(topic,payload=message,qos=1)
+	sleep(1)
+	count = count + 1
+
+
+
---
+++ mqtt-subscriber.py
@@ -0,0 +1,45 @@
+import paho.mqtt.client as mqtt
+
+# define static variable
+# broker = "localhost" # for local connection
+broker = "broker.hivemq.com"  # for online version
+port = 1883
+timeout = 60
+
+username = 'adara'
+password = '123'
+
+topic = "sic"
+ 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+ 
+    # Subscribing in on_connect() - if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(topic,qos=2)
+ 
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload.decode('utf-8')))
+    payload_decoded = msg.payload.decode('utf-8')
+    if payload_decoded == "on":
+        print("Lampu dinyalakan")
+        # Do something
+        
+    elif payload_decoded == "off":
+        print("Lampu dimatikan")
+        # Do something else
+    else:
+        print("perintah tidak di kenali")
+          
+        
+# Create an MQTT client and attach our routines to it.
+client = mqtt.Client("device0")
+client.username_pw_set(username=username,password=password)
+client.on_connect = on_connect
+client.on_message = on_message
+ 
+client.connect(broker, port, timeout)
+
+client.loop_forever()
